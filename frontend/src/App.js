5d57e2a02c620eea18fd275661ae0d5356286cba
yass <yass.srgrzg@gmail.com>
2024-05-21 11:29:20 +0200

    adding css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -68,6 +68,7 @@ function App() {
                             </Routes>
                         </div>
                         <Footer />
+
                     </UserProvider>
                 </Router>
 
